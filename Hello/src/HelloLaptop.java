4dddfd705f494aad22e40c63e0aa49803f52ad1c
Admin <bhagyashree6596@gmail.com>
2023-06-16 20:43:52 +0530

    Commit Laptop.Java

---
+++ Hello/src/HelloLaptop.java
@@ -0,0 +1,8 @@
+public class HelloLaptop {
+    public static void main(String[] args) {
+        String Lenovo;
+        System.out.println(" Lenovo Laptop");
+        String AppleMacBook;
+        System.out.println(" AppleMAcBook Laptop");
+    }
+}
