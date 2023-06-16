527363bf381267752a1e8fb9d0e394430e7b090c
Admin <bhagyashree6596@gmail.com>
2023-06-16 20:03:32 +0530

    Commit Farmer.java

---
+++ farmers/src/Farmer.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class Farmer {
+    public static void main(String[] args) {
+        String Prashant;
+        System.out.println("Prashant is Farmer");
+        String Mahesh;
+        System.out.println("Mahesh is Farmer");
+    }
+}
+
