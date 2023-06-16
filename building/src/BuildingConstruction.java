6e7eadfbc0ea99fbcfd1f1a30e210eaf4ca0c990
Admin <bhagyashree6596@gmail.com>
2023-06-16 20:08:14 +0530

    Commit BuildingConstruction.java

---
+++ building/src/BuildingConstruction.java
@@ -0,0 +1,9 @@
+public class BuildingConstruction {
+    public static void main(String[] args) {
+        String BigBuilding;
+        System.out.println("Big Building Floors");
+        String SmallBuilding;
+        System.out.println("Small Building Floors");
+
+    }
+}
