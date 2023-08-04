62f88519a9804c5dcdb08f735cb1176dbb2133a4
Admin <bhagyashree6596@gmail.com>
2023-08-04 08:08:28 +0530

    Java 8 Features 1. Functional Interface 2. Lambda Expression 3. Static And Default Method 4. Stream Example

--- primitive-non-primitive-data-type/java-8-features/src/streamexample/StreamExample.java
+++ primitive-non-primitive-data-type/java-8-features/src/streamexample/StreamExample.java
@@ -1,9 +1,10 @@
 package streamexample;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -30,17 +31,32 @@ class Student {
     public int getAge() {
         return age;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (!(o instanceof Student student)) return false;
+
+        return age == student.age
+                && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
 }
 public class StreamExample {
 
     public static void main(String[] args) {
         List<String> guestList = new ArrayList<>();
-        guestList.add("Dhanshree");
-        guestList.add("Prashant");
-        guestList.add("Rani");
+        guestList.add("Vikram");
+        guestList.add("Vivek");
+        guestList.add("Karan");
         guestList.add("Bhagyashree");
         guestList.add("Arti");
-        guestList.add("Dipali");
+        guestList.add("Moin");
 
         //Stream is sequence of elements -> elements can be anything like Integers, String , Student etc.
 
@@ -72,7 +88,7 @@ public class StreamExample {
                 .forEach(s3 -> System.out.println("s3 in the stream - " + s3));
 
         //if stream contains 5 elements then, for each element entire stream pipeline
-        //gets executed.
+        //gets executed one after the other.
 
         String[] friendList = new String[]{"Vikram", "Vivek", "Karan", "Moin"};
         //create stream using Arrays.stream() method.
@@ -83,22 +99,44 @@ public class StreamExample {
 
         //or create stream using Stream.of() method.
         System.out.println("##########################################################");
-        stringStream = Stream.of("Bhagyashree", "Rutuja", "Dipali");
+        stringStream = Stream.of("Vikram", "Mike", "Bob");
         stringStream.forEach(System.out::println);
 
 
 
         System.out.println("##########################################################");
         List<Student> studentList = new ArrayList<>();
-        studentList.add(new Student("Bhagyashree", 22));
-        studentList.add(new Student("Rutuja", 26));
-        studentList.add(new Student("Dipali", 28));
+        studentList.add(new Student("Vikram", 26));
+        studentList.add(new Student("Mike", 23));
+        studentList.add(new Student("Bob", 27));
+        studentList.add(new Student("Moin",23));
+        studentList.add(new Student("Aarti", 24));
+        studentList.add(new Student("Karan", 25));
 
         Stream<Student> studentStream = studentList.stream();
-        for (String name : studentStream.filter(s -> s.getAge() > 25).map(s -> s.getName()).collect(Collectors.toList())) {
+
+        //Lambda exp are nothing but they provide implementation for abstract method of func interface
+        //and at the same time lambda returns an object of func interface.
+        Predicate<Student> predicate = s -> s.getAge() > 24;//predicate does checking for given condition
+        Function<Student, String> mapper = s1 -> s1.getName();//function applies the transformation and returns a result
+        //filter -> filter out stream elements
+        //map -> transforms one stream to another stream
+//        System.out.println(studentStream
+//                .distinct()
+//                .count());//count() is terminal method
+//        System.out.println(studentStream//stream of Students
+//                .map(student -> student.getAge()) //Stream of Integers(ages)
+//                .max(Comparator.comparingInt(age -> age)));//Max from Stream of Integers using Comparator
+
+        for (String name : studentStream
+                .filter(predicate)
+                .map(mapper)
+                .collect(Collectors.toList())) {
             System.out.println(name);
         }
-
+        //if stream contains 5 elements then, for each element entire stream pipeline
+        //gets executed one after the other.
+        //This is because the Stream Pipeline is Executed Vertically.
 
     }
 }
